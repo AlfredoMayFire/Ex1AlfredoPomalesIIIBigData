05af7a62fc4da85210b221991124d71100e6ae8f
A_Pomales3 <{alfredopomales@gmail.com}>
2018-10-23 22:12:27 -0400

    perdon

--- Exercise1/src/main/java/Exercise1Reducer.java
+++ Exercise1/src/main/java/Exercise1Reducer.java
@@ -2,7 +2,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
-public class reducer extends Reducer<Text, Text, Text, Text> {
+public class Exercise1Reducer extends Reducer<Text, Text, Text, Text> {
     @Override
     protected void Exercise1Reducer(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
         StringBuilder myList = new StringBuilder();
